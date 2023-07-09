675dc151a9676aefd87b054ca369758e9016acf8
ARPAN MONDAL <accesstoarpan@gmail.com>
2023-07-10 00:52:34 +0530

    Update Identity.sol

--- Solidity/contracts/Identity.sol
+++ Solidity/contracts/Identity.sol
@@ -1,34 +1,54 @@
-// SPDX-Lisence-Identifier: MIT
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.17;
 
 contract Identity {
-    erum CHAINS {
-        INVALID,
-        EVM,
-        FLOW,
-        BSC,
-        TRON
-    }
+	enum CHAINS {
+		INVALID,
+		EVM,
+		FLOW,
+		BSC,
+		TRON
+	}
 
-    mapping(address =. mapping(CHAINS =. string)) public deligation;
-    mapping(string =. address[]) public deligationLookup;
-    mapping(string => mapping(address => unit256)) public lookupIndexes;
+	mapping(address => mapping(CHAINS => string)) public delegations;
+	mapping(string => address[]) public delegationsLookup;
+	mapping(string => mapping(address => uint256)) public lookupIndexes;
 
-    event DelegationSet(address _owner, CHAINS _chainID, String _accounts);
+	event DelegationSet(address _owner, CHAINS _chainId, string _account);
 
-    error InvalidChainId(CHAINS _chainId)
+	error InvalidChainId(CHAINS _chainId);
 
-    functions setDelegation(CHAINS _chainID, string calldata _account) eternal {
-        if (_chainID == CHAIN.INVALID) {
-            revert InvalidChainId(_chainId);
-        }
+	function setDelegation(CHAINS _chainId, string calldata _account) external {
+		if (_chainId == CHAINS.INVALID) {
+			revert InvalidChainId(_chainId);
+		}
 
-        string memory oldAccount = delegation[msg.sender][_chainId];
-        delegations[msg.sender][_chainId];
-        delegations[msg.sender[_chainId ]
-        
-        ]
-        }
-    }
+		string memory oldAccount = delegations[msg.sender][_chainId];
+		delegations[msg.sender][_chainId] = _account;
+
+		uint256 index = lookupIndexes[oldAccount][msg.sender];
+		delete lookupIndexes[oldAccount][msg.sender];
+
+		if (bytes(oldAccount).length > 0 && index > 0) {
+			delegationsLookup[oldAccount][index - 1] = address(0);
+		}
+
+		delegationsLookup[_account].push(msg.sender);
+		lookupIndexes[_account][msg.sender] = delegationsLookup[_account].length;
+
+		emit DelegationSet(msg.sender, _chainId, _account);
+	}
+
+	function getDelegation(address _owner, CHAINS _chainId) external view returns (string memory) {
+		return delegations[_owner][_chainId];
+	}
+
+	function getLookup(string calldata _account, uint256 _index) external view returns (address) {
+		return delegationsLookup[_account][_index];
+	}
+
+	function getLookupLength(string calldata _account) external view returns (uint256) {
+		return delegationsLookup[_account].length;
+	}
 }
